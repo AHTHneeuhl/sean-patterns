4608b2cb78000f23c253974cb91b296052b42954
Shubham Patrick <ahthneeuhl@gmail.com>
2022-07-26 17:36:43 +0530

    Missing Number | Python

---
+++ missingNumber.py
@@ -0,0 +1,25 @@
+# Time - O(N) | Space - O(N)
+def missingNumber(nums):
+    total, n = 0, len(nums)
+    for num in nums:
+        total += num
+    return (n * (n + 1) // 2) - total
+
+
+# Time - O(N) | Space - O(N)
+def missingNumber(nums):
+    n = len(nums)
+    res = n
+    for i in range(n):
+        res ^= i
+        res ^= nums[i]
+    return res
+
+
+if __name__ == '__main__':
+    nums1 = [3, 0, 1]
+    nums2 = [0, 1]
+    nums3 = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+    print(missingNumber(nums1))  # 2
+    print(missingNumber(nums2))  # 2
+    print(missingNumber(nums3))  # 8
