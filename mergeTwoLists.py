f987d72aea4b0fc33035c1aa723649d546bd52d6
Shubham Patrick <ahthneeuhl@gmail.com>
2022-08-14 11:29:20 +0530

    Merge Two Sorted Lists | Recursion

---
+++ mergeTwoLists.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
+
+
+def mergeTwoLists(list1, list2):
+    if not list1:
+        return list2
+    if not list2:
+        return list1
+    if list1.val < list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
